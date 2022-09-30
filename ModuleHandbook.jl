4dd6ca8102d21b39666cbb3c98811f9fdda03a74
mattatlincoln <mwatkins@lincoln.ac.uk>
2022-09-30 10:30:41 +0100

    tidy

---
+++ ModuleHandbook.jl
@@ -0,0 +1,119 @@
+### A Pluto.jl notebook ###
+# v0.19.12
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 84beabf0-3ff1-11ed-2042-55c4d4fe1606
+md"""
+# Module Handbook for Numerical Methods (MTH3007M)
+
+this document sets out the aims of the module, learning objectives and provides some use background material and suggestions.
+
+
+### Module synopsis
+
+"The module equips the students with knowledge of various numerical methods for solving applied mathematics problems, their algorithms and implementation in  programming languages."
+"""
+
+
+# ╔═╡ 511c9205-5645-4f51-8437-f57b43568399
+md"""
+## Overview
+
+- Learning objectives
+- Assessment
+- Deadlines
+- Logbooks
+- Module Contents
+- Tools
+"""
+
+
+# ╔═╡ 9890f9f8-a946-4574-a5b1-096da16e13f6
+md"""
+## Learning Objectives
+
+- LO1: Apply numerical methods for curve fitting and solving various equations.
+
+- LO2:Critically analyse applied mathematical problems, choose and apply appropriate numerical methods for their solution.
+
+- LO3: Implement numerical solutions in efficient computer codes using a high level computer language.
+"""
+
+
+# ╔═╡ a5eb34d9-46e2-4f32-86b0-1e74948b35ef
+md"""
+## Assessments
+
+This module will have two assignments at the end of each semester
+
+### Assignments
+assignments will consist of mini projects to be tackled over a period of a few days e.g. construct a predictive model from this data using methods you have seen in the module. They will be marked on an extended rubric detailing requirements for each degree classification:
+
+- Clear explanation of the problem addressed in mathematical terms
+- Explanation of the choice of method applied
+- Details of the numerical method implementation in your chosen language
+- Quality of the code - including comments, and testing.
+- Presentation of results and analysis
+
+A full rubric will be provided and examples given leading up to the assignments.
+
+## Deadlines
+
+There will be two assignments. They will be towards the end semester 1 and 2 you can see the times on the assessment timetable on Bb for all your modules.
+
+## Logbooks
+
+Logbooks will not be directly assessed in this module.
+
+!!! note
+we really do recommend that you keep a logbook for your own benefit. It is good professional practice and ... they will help you a lot in completing the assignments in a reasonable amount of time.
+
+Also, when submitting work you should attach evidence of your working, which would be very similar to relevant pages from your logbook.
+
+## Module Contents
+
+- Curve fitting.
+- Numerical solution of non-linear equations.
+- Numerical solution of ordinary differential equations.
+- Solving systems of equations.
+- Numerical solution of partial differential equations.
+- Elements of stochastic methods.
+
+## Tools
+
+in principle you can use any high level programming language as the focus is on understanding the numerical methods not the computing.
+
+- we recommend and support the use of Python
+- you can use C++ but this will be rather more difficult (in my opinion)
+- To work with C++ you can use MS Visual Studio which is installed in the computer labs and also is freely downloadable from [here](https://visualstudio.microsoft.com/). There are many other options and depending on the power of your computer and operating system there may be better options - ask us.
+- to work with Python I recommend Anaconda python which can be downloaded from [here](https://www.anaconda.com/products/individual) - click on the download button then pick the correct installer. I like using Jupyter notebooks – see the notebooks on Blackboard for reminder on how to use them - but there are many other nice working environments.
+
+You can use either of these languages – it would be very beneficial for you to see what parts are essential and what are trivial details of a particular language to try and use both. Would be good for your CV too.
+"""
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.1"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─84beabf0-3ff1-11ed-2042-55c4d4fe1606
+# ╟─511c9205-5645-4f51-8437-f57b43568399
+# ╟─9890f9f8-a946-4574-a5b1-096da16e13f6
+# ╟─a5eb34d9-46e2-4f32-86b0-1e74948b35ef
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
